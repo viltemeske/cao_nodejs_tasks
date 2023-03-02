d66679ae47093641c7c45116c7260b1e03858dd4
viltemeske <meshkenai@gmail.com>
2023-03-02 12:15:16 +0200

    completed task 4 part 2, added postman collections

---
+++ CAO_task_4_2/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+const data = require("./data.json")
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// 1.  Bendrinis GET route, kuris paduos visus duomenis
+
+app.get("/people", (req, res) => {
+    res.status(200).send(data);
+  });
+
+  // 2. Dinaminis GET route, kur URL turės automobilio markę ir pagal ją prafiltruos, ir grąžins tik tuos žmones, kurie turi šį automobilį.
+
+app.get("/cars/:brand", (req, res) => {
+    const brand = req.params.brand;
+    const findOwners = data.people.filter(person => person.car === brand);
+    
+    if(findOwners) {
+        res.status(200).send(findOwners);
+    } else {
+        res.status(404).send('This car brand was not found');
+    }
+});
+
+// 3. Dinamins GET route, kuris priims vartotojo id ir pagal jį grąžins atitinkamą vartotojo objektą.
+
+app.get('/people/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = data.people.find(person => person.id === id);
+  
+    if (!person) {
+      return res.status(404).send(`There is no person with id: '${id}'`);
+    }
+  
+    res.status(200).send(person);
+  });
+
+  // 4. GET route, kuris grąžins visus el. paštus.
+
+  app.get('/emails', (req, res) => {
+    const emails = data.people.map(item => item.email);
+    res.status(200).send(emails);
+  });
+
+  // 5. GET route, į kurį pasikreipus, grąžins visų moterų (gender: Female) vardą ir pavardę (formatas: ["Rita Kazlauskaite", "Monika Simaskaite"]).
+
+  app.get('/females', (req, res) => {
+    const female = data.people.filter(person => person.gender === 'Female');
+    const fullNames = female.map(fem => `${fem.first_name} ${fem.last_name}`);
+    res.status(200).json(fullNames);
+  });
+  
+app.listen(8081, () => console.log("The server is running on port 8081"));
+  
+  

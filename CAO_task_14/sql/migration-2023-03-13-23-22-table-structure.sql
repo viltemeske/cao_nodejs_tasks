1a5ede3884d90e40d0dc930f280120b13cbfe370
viltemeske <meshkenai@gmail.com>
2023-03-14 13:14:00 +0200

    added migration files

--- CAO_task_14/sql/migration-2023-03-13-23-22-table-structure.sql
+++ CAO_task_14/sql/migration-2023-03-13-23-22-table-structure.sql
@@ -1,8 +1,8 @@
-create table shirt2 (
+create table shirt (
 	shirtId int4 unsigned primary key auto_increment,
 	brand text not null,
     model text not null,
-    size text('XS', 'S', 'M', 'L', 'XL') default 'XS',
+    size enum('XS', 'S', 'M', 'L', 'XL') default 'XS',
     price decimal(4, 2) not null,
 	cretedAt timestamp default current_timestamp,
 	updatedAt timestamp default current_timestamp on update current_timestamp
